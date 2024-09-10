81468e7879c7fbc28a7cd6dd6f5b6cc960859b94
Nicolas <npereyramurray@gmail.com>
2024-09-10 00:47:28 -0300

    .

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,3 +1,5 @@
+import enablegif from "../assets/enable.svg"
+
 function Project(props) {
   return (
     <div className="project">
@@ -6,7 +8,7 @@ function Project(props) {
         <h4 className="project-title">{props.title}</h4>
         <div className="project-buttons">
           <a href="#" className="btn">
-            Live demo
+            Live demo <img src={enablegif} alt="enablegif" width="20px" height="20px" />
           </a>
           <a href="#" className="btn">
             View code
