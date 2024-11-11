b1b2d25d82a7f516e43b294657198c9b47a7217e
Nicolas <npereyramurray@gmail.com>
2024-11-11 00:43:16 -0300

    a

--- src/data.jsx
+++ src/data.jsx
@@ -1,13 +1,15 @@
-import { FaReact , FaJs } from "react-icons/fa";
+import { FaReact , FaNodeJs  } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+
 
 const projects = [
     {
-      title: "Project 1",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      title: "Mini Messagin App",
+      description: "A simple web application where users can add messages to a shared feed. All messages are visible to everyone in real-time, making it a great platform for quick announcements, group chats, or leaving notes. Ideal for small teams or communities looking for a lightweight messaging solution.",
       image:
-        "https://cdn.pixabay.com/photo/2015/11/10/08/31/banner-1036483_640.jpg",
-      link:"#",
-      stack: [<FaReact className="stack-icon"/> , <FaJs className="stack-icon"/>],
+        "https://i.postimg.cc/g2MNx20y/messagesapp.jpg",
+      link:"https://lucky-ganache-b3c2c1.netlify.app/",
+      stack: [<FaReact className="stack-icon"/> , <FaNodeJs  className="stack-icon"/> , <BiLogoPostgresql className="stack-icon"/> ],
     },
     {
       title: "Project 2",
@@ -15,7 +17,7 @@ const projects = [
       image:
         "https://static.vecteezy.com/system/resources/thumbnails/005/715/816/small/banner-abstract-background-board-for-text-and-message-design-modern-free-vector.jpg",
       link:"#",
-      stack: [<FaReact className="stack-icon"/> , <FaJs className="stack-icon"/>],
+      stack: [<FaReact className="stack-icon"/> , <FaNodeJs  className="stack-icon"/>],
     },
    
   ];
